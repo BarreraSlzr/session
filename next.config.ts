1583d0f1086011132207472136c054ff970cedbf
Emmanuel Barrera <barreraslzr@gmail.com>
2025-01-12 21:29:44 -0600

    Update package name and NextJS version in `package.json`
    * Change package name to `internetfriends-auth`
    * Change NextJS version to `15.1.4`
    
    Remove ppr experiment in `next.config.ts`
    
    * Comment out `ppr: true` in the experimental section

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   /* config options here */
   experimental: {
-    ppr: true,
+    // ppr: true,
     // serverActions: { allowedOrigins: [ "localhost:3000", "https://bug-free-invention-pj4qg57wvqh7p77-3000.app.github.dev/", ]}
   },
   images: {
