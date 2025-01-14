2de1f4d91e1f63f637224b587de9ba784fea00ad
Emmanuel Barrera <barreraslzr@gmail.com>
2025-01-14 05:52:02 +0000

    Reuse AuthConfig

--- app/(auth)/auth.ts
+++ app/(auth)/auth.ts
@@ -1,11 +1,5 @@
-import { compare } from 'bcrypt-ts';
+import { authConfig } from '@/app/(auth)/auth.config';
 import NextAuth, { type User, type Session } from 'next-auth';
-import Credentials from 'next-auth/providers/credentials';
-
-import { getUser } from '@/app/(auth)/db/queries';
-
-import { authConfig } from './auth.config';
-import { createSession, validateSession, renewSession, deleteSession } from '@/app/(auth)/lib/session';
 
 interface ExtendedSession extends Session {
   user: User;
@@ -17,44 +11,5 @@ export const {
   signIn,
   signOut,
 } = NextAuth({
-  ...authConfig,
-  providers: [
-    Credentials({
-      credentials: {},
-      async authorize({ email, password }: any) {
-        const users = await getUser(email);
-        if (users.length === 0) return null;
-        // biome-ignore lint: Forbidden non-null assertion.
-        const passwordsMatch = await compare(password, users[0].password!);
-        if (!passwordsMatch) return null;
-
-        const sessionToken = await createSession(users[0].id);
-        return { ...users[0], sessionToken } as any;
-      },
-    }),
-  ],
-  callbacks: {
-    async jwt({ token, user }) {
-      if (user) {
-        token.id = user.id;
-        token.sessionToken = user.sessionToken;
-      }
-
-      return token;
-    },
-    async session({
-      session,
-      token,
-    }: {
-      session: ExtendedSession;
-      token: any;
-    }) {
-      if (session.user) {
-        session.user.id = token.id as string;
-        session.user.sessionToken = token.sessionToken as string;
-      }
-
-      return session;
-    },
-  },
+  ...authConfig
 });
